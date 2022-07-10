0360e64db1fd92b989b1f7116450b2713dcb6bb5
Min Myat Oo <myat.o@pomelofashion.com>
2022-07-10 21:21:38 +0700

    fix: fix description

--- release.config.js
+++ release.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-    branches: "master",
-    repositoryURL: "https://github.com/minmyatoo/react-app/",
-    plugins:['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
-}
+  branches: "master",
+  repositoryURL: "https://github.com/minmyatoo/react-app/",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github"
+  ]
+};
