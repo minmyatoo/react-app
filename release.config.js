222409b26c0381365a0949c97d96639dc18e2a30
Min Myat Oo <myat.o@pomelofashion.com>
2022-07-10 22:11:40 +0700

    fix: Zip files and prettier

--- release.config.js
+++ release.config.js
@@ -5,15 +5,20 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
-    ["@semantic-release/github",{
-      assets:[
-        {
-          path: "build.zip", label:"Build"
-        },
-        {
-          path: "coverage.zip", label:"Coverage"
-        }
-      ]
-    }]
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: "build.zip",
+            label: "Build"
+          },
+          {
+            path: "coverage.zip",
+            label: "Coverage"
+          }
+        ]
+      }
+    ]
   ]
 };
